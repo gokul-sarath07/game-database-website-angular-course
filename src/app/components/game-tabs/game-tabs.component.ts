1369ce6e6facfb8efcbc4936502ceb095209351e
Gokul Sarath <gokul510.s@gmail.com>
2022-04-15 01:05:24 +0530

    Game tabs completed.

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/models';
 
 @Component({
   selector: 'app-game-tabs',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class GameTabsComponent implements OnInit {
 
   constructor() { }
+
+  @Input() game!: Game;
 
   ngOnInit(): void {
   }
